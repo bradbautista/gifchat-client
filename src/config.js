9f58ceaaa8ef81ed32e26ce8739dc0077f1a6012
unknown <bradbautista@gmail.com>
2020-02-11 22:25:16 -0500

    switched endpoint to heroku server for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  GIFCHAT_API_ENDPOINT: 'http://localhost:17043/',
+  GIFCHAT_API_ENDPOINT: 'https://blooming-ocean-65200.herokuapp.com/',
   TENOR_API_ENDPOINT: 'https://api.tenor.com/v1/',
   API_KEY: process.env.REACT_APP_API_KEY,
-  TOKEN_KEY: 'blogful-client-auth-token',
 }
