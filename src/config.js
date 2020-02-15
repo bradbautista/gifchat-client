2d95b0338e0ce638ef90945027967aec30f298dc
unknown <bradbautista@gmail.com>
2020-02-15 01:11:07 -0500

    pointed client back to prod server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  GIFCHAT_API_ENDPOINT: 'http://localhost:17043/',
-  // GIFCHAT_API_ENDPOINT: 'https://blooming-ocean-65200.herokuapp.com/',
+  // GIFCHAT_API_ENDPOINT: 'http://localhost:17043/',
+  GIFCHAT_API_ENDPOINT: 'https://blooming-ocean-65200.herokuapp.com/',
   TENOR_API_ENDPOINT: 'https://api.tenor.com/v1/',
   API_KEY: process.env.REACT_APP_API_KEY,
 }
